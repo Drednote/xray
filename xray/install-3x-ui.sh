6c5c2cce8bb07c78160d981fb3e6acc823efdca3
Ivan Galushko <galushko.ivan8@gmail.com>
2024-09-19 23:35:22 +0300

    Update install-3x-ui.sh

--- xray/install-3x-ui.sh
+++ xray/install-3x-ui.sh
@@ -1,8 +1,5 @@
 bash <(curl -sSL https://raw.githubusercontent.com/hamid-gh98/x-ui-scripts/main/install_warp_proxy.sh)
 
-git clone https://github.com/Drednote/shadow-rocket.git
-cd shadow-rocket || exit 0
-cd xray || exit 0
 docker-compose up -d
 
 openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout private.key -out public.key -days 3650 -subj "/CN=APP"

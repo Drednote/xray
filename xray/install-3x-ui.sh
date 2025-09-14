f00cd8af495ca9a5861e0b6c556021821489e1af
Ivan Galushko <galushko.ivan8@gmail.com>
2025-09-14 17:20:47 +0300

    Update install-3x-ui.sh

--- xray/install-3x-ui.sh
+++ xray/install-3x-ui.sh
@@ -6,3 +6,9 @@ docker-compose up -d
 openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout private.key -out public.key -days 3650 -subj "/CN=APP"
 docker cp private.key 3x-ui:private.key
 docker cp public.key 3x-ui:public.key
+
+# nano /etc/sysctl.conf
+# net.ipv6.conf.all.disable_ipv6 = 1
+# net.ipv6.conf.default.disable_ipv6 = 1
+# net.ipv6.conf.lo.disable_ipv6 = 1
+# sysctl -p
